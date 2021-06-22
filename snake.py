fb5663e40fd9b61ee305288e62147cac1ec0d485
JamesATrevino <jamesa.trevino@gmail.com>
2021-06-22 15:45:35 -0700

    use a naive approach for snake
    ditch the vision approach in favor of a naive danger proximity system. after 100000 iterations we manage an average score of 18

--- snake.py
+++ snake.py
@@ -1,30 +1,19 @@
-# Simple pygame program
-
-# Import and initialize the pygame library
-import random
-
 import gym
 import pygame
 import numpy as np
-import tensorflow as tf
 from rl.agents import SARSAAgent
-from rl.policy import EpsGreedyQPolicy
+from rl.policy import BoltzmannQPolicy
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import MeanSquaredError
 from rl.core import Env
-
-
-
 
 
 def build_model():
     model = Sequential()
-    model.add(Flatten(input_shape=(1, 28)))
-    model.add(Dense(18, activation='relu'))
-    model.add(Dense(18, activation='relu'))
-    model.add(Dense(4, activation='linear'))
+    model.add(Flatten(input_shape=(1, 11)))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(3, activation='linear'))
     return model
 
 
@@ -111,138 +100,6 @@ class Snake(object):
     def grow(self):
         self.segments.append({"x": self.segments[-1]["x"], "y": self.segments[-1]["y"]})
 
-    def get_vision_nw(self, board):
-        x_pos, y_pos = self.get_head()['x'], self.get_head()['y']
-        wall_dist = 0
-        snake_dist = 0
-        food_dist = 0
-        while x_pos > 0 and y_pos > 0:
-            y_pos -= 1
-            x_pos -= 1
-            wall_dist += 2
-            cell = board[x_pos][y_pos]
-            if cell == "SNAKE" and snake_dist == 0:
-                snake_dist = wall_dist
-            elif cell == "FOOD" and food_dist == 0:
-                food_dist = wall_dist
-
-        return wall_dist, snake_dist, food_dist
-
-    def get_vision_n(self, board):
-        x_pos, y_pos = self.get_head()['x'], self.get_head()['y']
-        wall_dist = 0
-        snake_dist = 0
-        food_dist = 0
-        while y_pos > 0:
-            y_pos -= 1
-            wall_dist += 1
-            cell = board[x_pos][y_pos]
-            if cell == "SNAKE" and snake_dist == 0:
-                snake_dist = wall_dist
-            elif cell == "FOOD" and food_dist == 0:
-                food_dist = wall_dist
-
-        return wall_dist, snake_dist, food_dist
-
-    def get_vision_ne(self, board, board_size):
-        x_pos, y_pos = self.get_head()['x'], self.get_head()['y']
-        wall_dist = 0
-        snake_dist = 0
-        food_dist = 0
-        while x_pos < (board_size - 1) and y_pos > 0:
-            y_pos -= 1
-            x_pos += 1
-            wall_dist += 2
-            cell = board[x_pos][y_pos]
-            if cell == "SNAKE" and snake_dist == 0:
-                snake_dist = wall_dist
-            elif cell == "FOOD" and food_dist == 0:
-                food_dist = wall_dist
-
-        return wall_dist, snake_dist, food_dist
-
-    def get_vision_e(self, board, board_size):
-        x_pos, y_pos = self.get_head()['x'], self.get_head()['y']
-        wall_dist = 0
-        snake_dist = 0
-        food_dist = 0
-        while x_pos < (board_size - 1):
-            x_pos += 1
-            wall_dist += 1
-            cell = board[x_pos][y_pos]
-            if cell == "SNAKE" and snake_dist == 0:
-                snake_dist = wall_dist
-            elif cell == "FOOD" and food_dist == 0:
-                food_dist = wall_dist
-
-        return wall_dist, snake_dist, food_dist
-
-    def get_vision_se(self, board, board_size):
-        x_pos, y_pos = self.get_head()['x'], self.get_head()['y']
-        wall_dist = 0
-        snake_dist = 0
-        food_dist = 0
-        while x_pos < (board_size - 1) and y_pos < (board_size - 1):
-            y_pos += 1
-            x_pos += 1
-            wall_dist += 2
-            cell = board[x_pos][y_pos]
-            if cell == "SNAKE" and snake_dist == 0:
-                snake_dist = wall_dist
-            elif cell == "FOOD" and food_dist == 0:
-                food_dist = wall_dist
-
-        return wall_dist, snake_dist, food_dist
-
-    def get_vision_s(self, board, board_size):
-        x_pos, y_pos = self.get_head()['x'], self.get_head()['y']
-        wall_dist = 0
-        snake_dist = 0
-        food_dist = 0
-        while y_pos < (board_size):
-            y_pos += 1
-            wall_dist += 1
-            cell = board[x_pos][y_pos]
-            if cell == "SNAKE" and snake_dist == 0:
-                snake_dist = wall_dist
-            elif cell == "FOOD" and food_dist == 0:
-                food_dist = wall_dist
-
-        return wall_dist, snake_dist, food_dist
-
-    def get_vision_sw(self, board, board_size):
-        x_pos, y_pos = self.get_head()['x'], self.get_head()['y']
-        wall_dist = 0
-        snake_dist = 0
-        food_dist = 0
-        while x_pos > 0 and y_pos < (board_size - 1):
-            y_pos += 1
-            x_pos -= 1
-            wall_dist += 2
-            cell = board[x_pos][y_pos]
-            if cell == "SNAKE" and snake_dist == 0:
-                snake_dist = wall_dist
-            elif cell == "FOOD" and food_dist == 0:
-                food_dist = wall_dist
-
-        return wall_dist, snake_dist, food_dist
-
-    def get_vision_w(self, board):
-        x_pos, y_pos = self.get_head()['x'], self.get_head()['y']
-        wall_dist = 0
-        snake_dist = 0
-        food_dist = 0
-        while x_pos > 0:
-            x_pos -= 1
-            wall_dist += 1
-            cell = board[x_pos][y_pos]
-            if cell == "SNAKE" and snake_dist == 0:
-                snake_dist = wall_dist
-            elif cell == "FOOD" and food_dist == 0:
-                food_dist = wall_dist
-
-        return wall_dist, snake_dist, food_dist
-
 
 class SnakeEnv(Env):
     def __init__(self, board_size=15):
@@ -250,11 +107,9 @@ class SnakeEnv(Env):
         self.viewer = None
         self.snake = Snake(int(board_size / 2), int(board_size / 2), 2)
         self.food_x, self.food_y = self.new_food()
-        self.state = self.get_vision()
+        self.state = self.get_state()
         self.steps_left = 300
-        self.action_space = gym.spaces.Discrete(4)
-
-
+        self.action_space = gym.spaces.Discrete(3)
 
     def step(self, action):
         err_msg = "%r (%s) invalid" % (action, type(action))
@@ -262,13 +117,9 @@ class SnakeEnv(Env):
 
         # apply sampled action
         if action == 0:
-            self.snake.move_north()
+            self.snake.turn_left()
         elif action == 1:
-            self.snake.move_east()
-        elif action == 2:
-            self.snake.move_south()
-        elif action == 3:
-            self.snake.move_west()
+            self.snake.turn_right()
 
         self.snake.move()
         head_x = self.snake.get_head()['x']
@@ -276,15 +127,15 @@ class SnakeEnv(Env):
         # out of bounds
         if (head_x == self.board_size) or (head_x == -1):
             done = True
-            reward = -1
+            reward = -10
         elif (head_y == self.board_size) or (head_y == -1):
             done = True
-            reward = -1
+            reward = -10
         # found food
         elif head_x == self.food_x and head_y == self.food_y:
             self.snake.grow()
             self.food_x, self.food_y = self.new_food()
-            reward = 1
+            reward = 10
             done = False
             self.steps_left = 300
         else:
@@ -295,18 +146,18 @@ class SnakeEnv(Env):
         for segment in self.snake.get_segments()[1:]:
             if head_x == segment['x'] and head_y == segment['y']:
                 done = True
-                reward = -1
+                reward = -10
 
         if self.steps_left == 0:
             done = True
 
-        self.state = self.get_vision()
-        return self.state, reward, done, {}
+        self.state = self.get_state()
+        return self.state, reward, done, {'score': self.snake.get_length() - self.snake.initial_length}
 
     def reset(self):
         self.snake = Snake(int(self.board_size / 2), int(self.board_size / 2), 2)
         self.food_x, self.food_y = self.new_food()
-        self.state = self.get_vision()
+        self.state = self.get_state()
         self.steps_left = 300
         return self.state
 
@@ -389,53 +240,39 @@ class SnakeEnv(Env):
 
         print("\n")
 
-    def get_vision(self, debug=False):
+    def get_state(self):
         board = self.construct_board()
+        direction = self.snake.get_direction()
+        head_x = self.snake.get_head()['x']
+        head_y = self.snake.get_head()['y']
 
-        wall_nw, snake_nw, food_nw = self.snake.get_vision_nw(board)
-        wall_n, snake_n, food_n = self.snake.get_vision_n(board)
-        wall_ne, snake_ne, food_ne = self.snake.get_vision_ne(board, self.board_size)
-        wall_e, snake_e, food_e = self.snake.get_vision_e(board, self.board_size)
-        wall_se, snake_se, food_se = self.snake.get_vision_se(board, self.board_size)
-        wall_s, snake_s, food_s = self.snake.get_vision_s(board, self.board_size)
-        wall_sw, snake_sw, food_sw = self.snake.get_vision_sw(board, self.board_size)
-        wall_w, snake_w, food_w = self.snake.get_vision_w(board)
-        facing_n = 1 if self.snake.get_direction() == 'NORTH' else 0
-        facing_e = 1 if self.snake.get_direction() == 'EAST' else 0
-        facing_s = 1 if self.snake.get_direction() == 'SOUTH' else 0
-        facing_w = 1 if self.snake.get_direction() == 'WEST' else 0
+        if direction == 'NORTH':
+            danger_ahead = head_y == 0 or board[head_x][head_y - 1] == 'SNAKE'
+            danger_left = head_x == 0 or board[head_x - 1][head_y] == 'SNAKE'
+            danger_right = head_x == self.board_size or board[head_x + 1][head_y] == 'SNAKE'
+        elif direction == 'EAST':
+            danger_ahead = head_x == self.board_size or board[head_x + 1][head_y] == 'SNAKE'
+            danger_left = head_y == 0 or board[head_x][head_y - 1] == 'SNAKE'
+            danger_right = head_y == self.board_size or board[head_x][head_y + 1] == 'SNAKE'
+        elif direction == 'SOUTH':
+            danger_ahead = head_y == self.board_size or board[head_x][head_y + 1] == 'SNAKE'
+            danger_left = head_x == 0 or board[head_x - 1][head_y] == 'SNAKE'
+            danger_right = head_x == self.board_size or board[head_x + 1][head_y] == 'SNAKE'
+        else:
+            danger_ahead = head_x == 0 or board[head_x - 1][head_y] == 'SNAKE'
+            danger_left = head_y == self.board_size or board[head_x][head_y + 1] == 'SNAKE'
+            danger_right = head_y == 0 or board[head_x][head_y - 1] == 'SNAKE'
 
-        if debug:
-            self.print_board(board)
+        facing_n = direction == 'NORTH'
+        facing_e = direction == 'EAST'
+        facing_s = direction == 'SOUTH'
+        facing_w = direction == 'WEST'
+        food_west = self.food_x < self.snake.get_head()['x']
+        food_north = self.food_y < self.snake.get_head()['y']
+        food_east = self.food_x > self.snake.get_head()['x']
+        food_south = self.food_y > self.snake.get_head()['y']
 
-            s = 'wall_nw %d, snake_nw %d, food_nw %d, ' \
-                '\nwall_n %d, snake_n %d, food_n %d, ' \
-                '\nwall_ne %d, snake_ne %d, food_ne %d, ' \
-                '\nwall_e %d, snake_e %d, food_e %d, ' \
-                '\nwall_se %d, snake_se %d, food_se %d, ' \
-                '\nwall_s %d, snake_s %d, food_s %d, ' \
-                '\nwall_sw %d, snake_sw %d, food_sw %d, ' \
-                '\nwall_w %d, snake_w %d, food_w %d, ' % \
-                (wall_nw, snake_nw, food_nw,
-                 wall_n, snake_n, food_n,
-                 wall_ne, snake_ne, food_ne,
-                 wall_e, snake_e, food_e,
-                 wall_se, snake_se, food_se,
-                 wall_s, snake_s, food_s,
-                 wall_sw, snake_sw, food_sw,
-                 wall_w, snake_w, food_w,
-                 )
-            print(s)
-
-        return np.array([wall_nw, snake_nw, food_nw,
-                         wall_n, snake_n, food_n,
-                         wall_ne, snake_ne, food_ne,
-                         wall_e, snake_e, food_e,
-                         wall_se, snake_se, food_se,
-                         wall_s, snake_s, food_s,
-                         wall_sw, snake_sw, food_sw,
-                         wall_w, snake_w, food_w,
-                         facing_n, facing_e, facing_s, facing_w])
+        return np.array([danger_ahead, danger_left, danger_right, facing_n, facing_e, facing_s, facing_w, food_west, food_north, food_east, food_south], dtype=int)
 
     def new_food(self):
         import itertools
@@ -451,13 +288,13 @@ class SnakeEnv(Env):
 def run():
     env = SnakeEnv(15)
     model = build_model()
-    policy = EpsGreedyQPolicy()
+    policy = BoltzmannQPolicy()
     sarsa = SARSAAgent(model=model, policy=policy, nb_actions=env.action_space.n)
-    sarsa.compile('adam', metrics=['mse'])
-    sarsa.fit(env, nb_steps=1000000, visualize=False, verbose=1)
+    sarsa.compile(Adam(), metrics=['mse'])
+    sarsa.fit(env, nb_steps=100000, visualize=False, verbose=1)
+    sarsa.save_weights('sarsa_snake_naive_3_moves.h5f', overwrite=True)
     scores = sarsa.test(env, nb_episodes=100, visualize=True, verbose=1)
     print('Average score over 100 test games:{}'.format(np.mean(scores.history['episode_reward'])))
-    sarsa.save_weights('sarsa_snake_weights.h5f', overwrite=True)
 
 
 if __name__ == "__main__":
