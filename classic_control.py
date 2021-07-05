37d9739cb7e11c04084b7b3f30c17aef4e773b84
JamesATrevino <jamesa.trevino@gmail.com>
2021-07-05 08:42:56 -0700

    clean up classic control example

--- classic_control.py
+++ classic_control.py
@@ -1,12 +1,13 @@
 import gym
-import random
 import numpy as np
-from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
 from rl.agents import SARSAAgent
 from rl.policy import EpsGreedyQPolicy
 
-env = gym.make('CartPole-v0')
+# Taken from an open-ai tutorial
+env = gym.make('CartPole-v1')
 
 
 def agent(states, actions):
@@ -18,11 +19,14 @@ def agent(states, actions):
     model.add(Dense(actions, activation='linear'))
     return model
 
+
 model = agent(env.observation_space.shape[0], env.action_space.n)
 policy = EpsGreedyQPolicy()
+
 sarsa = SARSAAgent(model = model, policy = policy, nb_actions = env.action_space.n)
-sarsa.compile('adam', metrics = ['mse'])
-sarsa.fit(env, nb_steps = 50000, visualize = False, verbose = 1)
+sarsa.compile(optimizer=Adam(), metrics = ['mse'])
+#sarsa.fit(env, nb_steps = 100000, visualize = False, verbose = 1)
+sarsa.load_weights('sarsa_weights.h5f')
 scores = sarsa.test(env, nb_episodes = 100, visualize= False)
 print('Average score over 100 test games:{}'.format(np.mean(scores.history['episode_reward'])))
 #sarsa.save_weights('sarsa_weights.h5f', overwrite=True)
